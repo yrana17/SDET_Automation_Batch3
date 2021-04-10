fe83d40d762da5ce3b11a45c26ee5979e92a7c4f
YRANA <rana05.akki@gmail.com>
2021-04-10 18:51:38 +0530

    Case 1 continuation

--- src/test/java/casestudy1/HandleData.java
+++ src/test/java/casestudy1/HandleData.java
@@ -2,8 +2,11 @@ package casestudy1;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.HashMap;
 
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -23,18 +26,51 @@ public class HandleData {
 				wb = new HSSFWorkbook(fis);
 
 			Sheet sh = wb.getSheet(SheetName);
-
 			int rowcnt = sh.getPhysicalNumberOfRows();
-
 			System.out.println("Row Count:" + rowcnt);
-
 			for (int i = 0; i < rowcnt; i++) {
-
 				TestDataOrganization.hmInput.put(sh.getRow(i).getCell(0).toString(),
 						sh.getRow(i).getCell(1).toString());
 
 			}
 
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public void writeLocationTableData(String FileName, String SheetName) {
+
+		try {
+			File fl = new File(FileName);
+			FileInputStream inputStream = new FileInputStream(fl);
+			String fileEtensionName = FileName.substring(FileName.indexOf("."));
+			Workbook workbook = null;
+			if (fileEtensionName.equals(".xlsx"))
+				workbook = new XSSFWorkbook(inputStream);
+			else if (fileEtensionName.equals(".xls"))
+				workbook = new HSSFWorkbook(inputStream);
+
+			Sheet sh = workbook.getSheet(SheetName);
+
+			for (int i = 0; i < TestDataOrganization.listLocationTableDataUI.size(); i++) {
+				Row rw = sh.createRow(i);
+				HashMap<String, String> hm = TestDataOrganization.listLocationTableDataUI.get(i);
+
+				int col = 0;
+				for (String k : hm.keySet()) {
+
+					rw.createCell(col).setCellValue(hm.get(k));
+					col = col + 1;
+				}
+			}
+
+			inputStream.close();
+
+			FileOutputStream fos = new FileOutputStream(fl);
+			workbook.write(fos);
+			workbook.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
--- src/test/java/casestudy1/OrganizationLocationPage.java
+++ src/test/java/casestudy1/OrganizationLocationPage.java
@@ -1,5 +1,7 @@
 package casestudy1;
 
+import java.util.HashMap;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
@@ -13,6 +15,8 @@ public class OrganizationLocationPage {
 
 	By city = By.id("searchLocation_city");
 	By searchButton = By.id("btnSearch");
+	By locationTable = By.xpath("//*[@id='resultTable']");
+	By locationTableRow = By.xpath("//*[@id='resultTable']/tbody/tr");
 
 	public void setCityName(String strCity) {
 		driver.findElement(city).sendKeys(strCity);
@@ -20,6 +24,33 @@ public class OrganizationLocationPage {
 
 	public void clickOnSearchBtn() {
 		driver.findElement(searchButton).click();
+	}
+
+	public int getRowCnt() {
+		return driver.findElements(locationTableRow).size();
+	}
+
+	public void readTableData() {
+
+		int rw = getRowCnt();
+		for (int i = 2; i <= rw; i++) {
+
+			HashMap<String, String> hm = new HashMap<String, String>();
+
+			String val = driver.findElement(By.xpath("//*[@id='resultTable']/tbody/tr[" + i + "]/td[2]/a")).getText();
+			hm.put("Name", val);
+			val = driver.findElement(By.xpath("//*[@id='resultTable']/tbody/tr[" + i + "]/td[3]")).getText();
+			hm.put("City", val);
+			val = driver.findElement(By.xpath("//*[@id='resultTable']/tbody/tr[" + i + "]/td[4]")).getText();
+			hm.put("Country", val);
+			val = driver.findElement(By.xpath("//*[@id='resultTable']/tbody/tr[" + i + "]/td[5]")).getText();
+			hm.put("Phone", val);
+			val = driver.findElement(By.xpath("//*[@id='resultTable']/tbody/tr[" + i + "]/td[6]")).getText();
+			hm.put("NoOfEmployees", val);
+
+			TestDataOrganization.listLocationTableDataUI.add(hm);
+		}
+
 	}
 
 }
